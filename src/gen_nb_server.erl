3c1bdcb91654c8b6528fe12bec5a8009f57a9957
Kyle Neal <kyle@verias.com>
2016-10-21 11:55:43 -0400

    (feat) Bring gen_nb_server to date to current needed functionality

--- src/gen_nb_server.erl
+++ src/gen_nb_server.erl
@@ -25,168 +25,123 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/2,
-         start_link/3,
-         get_cb_state/1,
-         store_cb_state/2,
-         add_listen_socket/2,
-         remove_listen_socket/2]).
+-export([start_link/4, start_link/5]).
 
 %% Behavior callbacks
 -export([behaviour_info/1]).
 
 %% gen_server callbacks
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
 
 -record(state, {cb,
-                addrs=dict:new(),
-                socks=dict:new(),
+                sock,
                 server_state}).
 
 %% @hidden
 behaviour_info(callbacks) ->
-    [{init, 2},
+    [{init, 1},
      {handle_call, 3},
      {handle_cast, 2},
      {handle_info, 2},
      {terminate, 2},
      {sock_opts, 0},
-     {new_connection, 4}];
+     {new_connection, 2}];
 
 behaviour_info(_) ->
     undefined.
 
+%% @spec start_link({local, Name}, CallbackModule, IpAddr, Port, InitParams) -> Result
+%% Name = atom()
+%% CallbackModule = atom()
+%% IpAddr = string()
+%% Port = integer()
+%% InitParams = [any()]
+%% Result = {ok, pid()} | {error, any()}
 %% @doc Start server listening on IpAddr:Port
--spec start_link(atom(), [any()]) -> ok | ignore | {error, any()}.
-start_link(CallbackModule, InitParams) ->
-    gen_server:start_link(?MODULE, [CallbackModule, InitParams], [{fullsweep_after, 0}]).
+start_link({local, Name}, CallbackModule, IpAddr, Port, InitParams) ->
+    gen_server:start_link({local, Name}, ?MODULE, [CallbackModule, IpAddr, Port, InitParams], []).
 
+%% @spec start_link(CallbackModule, IpAddr, Port, InitParams) -> Result
+%% CallbackModule = atom()
+%% IpAddr = string()
+%% Port = integer()
+%% InitParams = [any()]
+%% Result = {ok, pid()} | {error, any()}
+%% @doc Start server listening on IpAddr:Port
+start_link(CallbackModule, IpAddr, Port, InitParams) ->
+    gen_server:start_link(?MODULE, [CallbackModule, IpAddr, Port, InitParams], []).
 
-%% @doc Start server listening on IpAddr:Port registered as Name
--spec start_link(atom(), atom(), [any()]) -> ok | ignore | {error, any()}.
-start_link(Name, CallbackModule, InitParams) ->
-    gen_server:start_link(Name, ?MODULE, [CallbackModule, InitParams], [{fullsweep_after, 0}]).
-
-%% @doc Extracts the callback module's state from the server's overall state
-%%      NOTE: Should only be called by the submodule
--spec get_cb_state(#state{}) -> any().
-get_cb_state(#state{server_state=SState}) ->
-    SState.
-
-%% @doc Stores the callback module's state into the server's state
-%%      NOTE: Should only be called by the submodule
--spec store_cb_state(any(), #state{}) -> #state{}.
-store_cb_state(CBState, State) when is_record(State, state) ->
-    State#state{server_state=CBState}.
-
-%% @doc Adds a new listener socket to be managed by gen_nb_server
-%%      NOTE: Should only be called by the submodule
--spec add_listen_socket({string(), integer()}, #state{}) -> {ok, #state{}} | {error, any()}.
-add_listen_socket({IpAddr, Port}, #state{cb=Callback, addrs=Addrs, socks=Socks}=State) ->
-    Key = {IpAddr, Port},
-    case dict:find(Key, Socks) of
-        {ok, _} ->
-            {error, already_listening};
-        error ->
-            case listen_on(Callback, IpAddr, Port) of
+%% @hidden
+init([CallbackModule, IpAddr, Port, InitParams]) ->
+    case CallbackModule:init(InitParams) of
+        {ok, ServerState} ->
+            case listen_on(CallbackModule, IpAddr, Port) of
                 {ok, Sock} ->
-                    {ok, State#state{socks=dict:store(Key, Sock, Socks),
-                                     addrs=dict:store(Sock, Key, Addrs)}};
+                    {ok, #state{cb = CallbackModule, sock = Sock, server_state = ServerState}};
                 Error ->
+                    CallbackModule:terminate(Error, ServerState),
                     Error
-            end
-    end.
-
-%% @doc Removes a new listener socket to be managed by gen_nb_server
-%%      NOTE: Should only be called by the submodule
--spec remove_listen_socket({string(), integer()}, #state{}) -> {error, not_listening} | {ok, #state{}}.
-remove_listen_socket({IpAddr, Port}, #state{socks=Socks, addrs=Addrs}=State) ->
-    Key = {IpAddr, Port},
-    case dict:find(Key, Socks) of
-        error ->
-            {error, not_listening};
-        {ok, Sock} ->
-            gen_tcp:close(Sock),
-            {ok, State#state{socks=dict:erase(Key, Socks),
-                             addrs=dict:erase(Sock, Addrs)}}
-    end.
-
-%% @doc Returns the callback module's state
--spec init([atom()|any()]) -> {ok, #state{}} | {error, bad_init_state} | {error, any()}.
-init([CallbackModule, InitParams]) ->
-    process_flag(trap_exit, true),
-    State = #state{cb=CallbackModule},
-    case CallbackModule:init(InitParams, State) of
-        {ok, ServerState} when is_record(ServerState, state) ->
-            {ok, ServerState};
-        {ok, _State} ->
-            {error, bad_init_state};
+            end;
         Err ->
             Err
     end.
 
 %% @hidden
-handle_call(Request, From, #state{cb=Callback}=State) ->
-    case Callback:handle_call(Request, From, State) of
+handle_call(Request, From, #state{cb = Callback, server_state = ServerState} = State) ->
+    case Callback:handle_call(Request, From, ServerState) of
         {reply, Reply, NewServerState} ->
-            {reply, Reply, NewServerState};
+            {reply, Reply, State#state{server_state = NewServerState}};
         {reply, Reply, NewServerState, Arg} when Arg =:= hibernate orelse is_number(Arg) ->
-            {reply, Reply, NewServerState, Arg};
+            {reply, Reply, State#state{server_state = NewServerState}, Arg};
         {noreply, NewServerState} ->
-            {noreply, NewServerState};
+            {noreply, State#state{server_state = NewServerState}};
         {noreply, NewServerState, Arg} when Arg =:= hibernate orelse is_number(Arg) ->
-            {noreply, NewServerState, Arg};
+            {noreply, State#state{server_state = NewServerState}, Arg};
         {stop, Reason, NewServerState} ->
-            {stop, Reason, NewServerState};
+            {stop, Reason, State#state{server_state = NewServerState}};
         {stop, Reason, Reply, NewServerState} ->
-            {stop, Reason, Reply, NewServerState}
+            {stop, Reason, Reply, State#state{server_state = NewServerState}}
     end.
 
 %% @hidden
-handle_cast(Msg, #state{cb=Callback}=State) ->
-    case Callback:handle_cast(Msg, State) of
+handle_cast(Msg, #state{cb = Callback, server_state = ServerState} = State) ->
+    case Callback:handle_cast(Msg, ServerState) of
         {noreply, NewServerState} ->
-            {noreply, NewServerState};
+            {noreply, State#state{server_state = NewServerState}};
         {noreply, NewServerState, Arg} when Arg =:= hibernate orelse is_number(Arg) ->
-            {noreply, NewServerState, Arg};
+            {noreply, State#state{server_state = NewServerState}, Arg};
         {stop, Reason, NewServerState} ->
-            {stop, Reason, NewServerState}
+            {stop, Reason, State#state{server_state = NewServerState}}
     end.
 
 %% @hidden
-handle_info({inet_async, ListSock, _Ref, {ok, CliSocket}}, #state{cb=Callback, addrs=Addrs}=State) ->
+handle_info({inet_async, ListSock, _Ref, {ok, CliSocket}}, #state{cb = Callback, server_state = ServerState} = State) ->
     inet_db:register_socket(CliSocket, inet_tcp),
-    {IpAddr, Port} = dict:fetch(ListSock, Addrs),
-    case Callback:new_connection(IpAddr, Port, CliSocket, State) of
+    case Callback:new_connection(CliSocket, ServerState) of
         {ok, NewServerState} ->
             prim_inet:async_accept(ListSock, -1),
-            {noreply, NewServerState};
+            {noreply, State#state{server_state = NewServerState}};
         {stop, Reason, NewServerState} ->
-            {stop, Reason, NewServerState}
+            {stop, Reason, State#state{server_state = NewServerState}}
     end;
 
-handle_info(Info, #state{cb=Callback}=State) ->
-    case Callback:handle_info(Info, State) of
+handle_info(Info, #state{cb = Callback, server_state = ServerState} = State) ->
+    case Callback:handle_info(Info, ServerState) of
         {noreply, NewServerState} ->
-            {noreply, NewServerState};
+            {noreply, State#state{server_state = NewServerState}};
         {noreply, NewServerState, Arg} when Arg =:= hibernate orelse is_number(Arg) ->
-            {noreply, NewServerState, Arg};
+            {noreply, State#state{server_state = NewServerState}, Arg};
         {stop, Reason, NewServerState} ->
-            {stop, Reason, NewServerState}
+            {stop, Reason, State#state{server_state = NewServerState}}
     end.
 
 %% @hidden
-terminate(Reason, #state{cb=Callback, addrs=Addrs}=State) ->
-    [gen_tcp:close(Sock) || Sock <- dict:fetch_keys(Addrs)],
-    State1 = State#state{addrs=dict:new(), socks=dict:new()},
-    Callback:terminate(Reason, State1),
+terminate(Reason, #state{cb = Callback, sock = Sock, server_state = ServerState}) ->
+    gen_tcp:close(Sock),
+    Callback:terminate(Reason, ServerState),
     ok.
 
 %% @hidden
@@ -197,12 +152,12 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% @hidden
 %% @spec listen_on(CallbackModule, IpAddr, Port) -> Result
-%%       CallbackModule = atom()
-%%       IpAddr = string()
-%%       Port = integer()
-%%       Result = {ok, port()} | {error, any()}
+%% CallbackModule = atom()
+%% IpAddr = string()
+%% Port = integer()
+%% Result = {ok, port()} | {error, any()}
 listen_on(CallbackModule, IpAddr, Port) ->
-    SockOpts = [{ip, convert(IpAddr)}|CallbackModule:sock_opts()],
+    SockOpts = [{ip, IpAddr} | CallbackModule:sock_opts()],
     case gen_tcp:listen(Port, SockOpts) of
         {ok, LSock} ->
             {ok, _Ref} = prim_inet:async_accept(LSock, -1),
@@ -210,12 +165,3 @@ listen_on(CallbackModule, IpAddr, Port) ->
         Err ->
             Err
     end.
-
-%% @hidden
-%% @spec convert(Addr) -> Result
-%%       Addr = string()
-%%       Result = {integer(), integer(), integer(), integer()}
-%% @doc Converts text IP addresses "0.0.0.0" to tuples {0, 0, 0, 0}
-convert(Addr) ->
-    T = string:tokens(Addr, "."),
-    list_to_tuple([list_to_integer(X) || X <- T]).
